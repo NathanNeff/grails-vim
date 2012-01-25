32a2c7d91e0318074a29c37c556f408682983c64
Nathan Neff <nathan.neff@gmail.com>
2012-01-25 15:05:22 -0600

    Start reading errors from last line read

--- bin/stackTraceParse.groovy
+++ bin/stackTraceParse.groovy
@@ -1,8 +1,20 @@
 #!/usr/bin/env groovy
-def lines = new File('target/stacktrace.log').readLines()
+def lastlineFile = 'target/stacktrace_lastline.log'
+def lastline = lastlineRead(lastlineFile)
+def lines = new File('logs/enrollio.log').readLines()
+lines = lines[lastline .. lines.size() -1]
 lines.each { line ->
     m = line =~ /^Caused by: \S+\s*(.*?)(\S+\d+)$/
     if (m.getCount() == 1) {
         println m[0][2] + ":" + m[0][1]
     }
+def lastlineRead(lastlineFile) {
+    def lastline = 0;
+    def fh = new File(lastlineFile)
+
+    if ( fh.exists() ) {
+        lastline = fh.readLines()[2]
+        fh.delete()
+    }
+    return lastline.toInteger()
 }
