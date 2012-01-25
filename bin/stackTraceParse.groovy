864d9fb8469869b864e5e9405565824fa0d9f05f
Nathan Neff <nathan.neff@gmail.com>
2012-01-25 15:05:50 -0600

    Remove current dir from path

--- bin/stackTraceParse.groovy
+++ bin/stackTraceParse.groovy
@@ -2,11 +2,15 @@
 def lastlineFile = 'target/stacktrace_lastline.log'
 def lastline = lastlineRead(lastlineFile)
 def lines = new File('logs/enrollio.log').readLines()
+def dir = new File(".").canonicalPath + "/"
+
 lines = lines[lastline .. lines.size() -1]
 lines.each { line ->
     m = line =~ /^Caused by: \S+\s*(.*?)(\S+\d+)$/
     if (m.getCount() == 1) {
-        println m[0][2] + ":" + m[0][1]
+        def relPath = m[0][2] - dir
+        println relPath + ":" + m[0][1]
+    }
     }
 def lastlineRead(lastlineFile) {
     def lastline = 0;
